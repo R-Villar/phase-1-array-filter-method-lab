3a00c3d71fc2a0fd631770d1fae45adab2dc9d38
Remberto Villar <rembertovillar@gmail.com>
2022-07-03 11:52:05 -0400

    commit tree e8738530d71de02f4deb39307e75d57fbc28b457

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(collection, result) {
+    return collection.filter(
+        (matches) => matches.toLowerCase() === result.toLowerCase()
+    );
+}
+
+function fuzzyMatch(collection, name) {
+    return collection.filter(
+        (matches) => matches.toLowerCase().indexOf(name.toLowerCase()) === 0
+    )
+}
+
+function matchName(collection, lookUpName) {
+    return collection.filter((record) => record.name === lookUpName)
+}
